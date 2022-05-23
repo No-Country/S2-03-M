8964455cc6621a1b41d4c2b50f997cbc241c01fb
Nazareno Gabriel Bastanzo <b.nazarenogabriel@gmail.com>
2022-05-23 09:46:04 -0300

    Frontend: Reactive Forms, First Approach

--- turismo-app/src/app/pages/home/home.component.ts
+++ turismo-app/src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  myForm!: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.myForm = this.initForm();
+  }
+
+  initForm(): FormGroup {
+    return this.formBuilder.group({
+      from: ['', [Validators.required]],
+      to: ['', [Validators.required]],
+      departure: ['', Validators.required],
+      return: ['', Validators.required],
+      trip: ['', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    console.log('On Submit ->', this.myForm.value);
+    
   }
 
 }
