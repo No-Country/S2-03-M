f9b431183048feef51fdc1e2aa00686e3e049b2d
Nazareno Gabriel Bastanzo <b.nazarenogabriel@gmail.com>
2022-05-23 13:10:49 -0300

    Frontend: Angular Material applied

--- turismo-app/src/app/modules/material/material.module.ts
+++ turismo-app/src/app/modules/material/material.module.ts
@@ -2,16 +2,23 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
 import { MatFormFieldModule } from "@angular/material/form-field";
-import {MatInputModule} from '@angular/material/input';
+import { MatInputModule } from "@angular/material/input";
 import { MatDatepickerModule } from "@angular/material/datepicker";
 import { MatNativeDateModule, MAT_DATE_LOCALE } from "@angular/material/core";
+import { MatRadioModule } from "@angular/material/radio";
+import { MatButtonModule } from "@angular/material/button";
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [MatDatepickerModule, MatNativeDateModule, MatFormFieldModule, MatInputModule],
-  providers: [
-    {provide: MAT_DATE_LOCALE, useValue: 'es-AR'},
+  exports: [
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRadioModule,
+    MatButtonModule
   ],
+  providers: [{ provide: MAT_DATE_LOCALE, useValue: "es-AR" }],
 })
 export class MaterialModule {}
