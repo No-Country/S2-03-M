7503aa59c98136283ce841535a342ab178a394c5
Nazareno Gabriel Bastanzo <b.nazarenogabriel@gmail.com>
2022-05-24 15:55:14 -0300

    Frontend: Small fix

--- turismo-app/src/app/pages/home/home.component.ts
+++ turismo-app/src/app/pages/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { BehaviorSubject, tap } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-home',
