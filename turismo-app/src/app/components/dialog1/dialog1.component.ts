22f2c502cf1eda7aaa76b91892b011df18564af5
Nazareno Gabriel Bastanzo <b.nazarenogabriel@gmail.com>
2022-05-29 18:41:06 -0300

    Frontend: small fix

--- turismo-app/src/app/components/dialog1/dialog1.component.ts
+++ turismo-app/src/app/components/dialog1/dialog1.component.ts
@@ -3,13 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-dialog1',
   templateUrl: './dialog1.component.html',
-  styleUrls: ['./dialog1.component.scss']
+  styleUrls: ['./dialog1.component.scss'],
 })
-export class Dialog1Component implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class Dialog1Component {
+  constructor() {}
 }
