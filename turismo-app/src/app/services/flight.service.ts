4f994a5f5fc4cbb064a570ded50c3cd4c3e14ffe
Nazareno Gabriel Bastanzo <b.nazarenogabriel@gmail.com>
2022-05-31 08:42:23 -0300

    Frontend: LocalStorage approach

--- turismo-app/src/app/services/flight.service.ts
+++ turismo-app/src/app/services/flight.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, BehaviorSubject } from 'rxjs';
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { FlightQuery } from '../interfaces/flight-query.interface';
 
 @Injectable({
   providedIn: 'root',
