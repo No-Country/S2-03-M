ac64decf805fbb22bdde4aae8ac97e450535379b
Nazareno Gabriel Bastanzo <b.nazarenogabriel@gmail.com>
2022-06-08 12:49:14 -0300

    Frontend: 1st approach on DB connect

--- turismo-app/src/app/components/sidenav/sidenav.component.ts
+++ turismo-app/src/app/components/sidenav/sidenav.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { OauthService } from '../../services/oauth.service';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { TokenService } from '../../services/token.service';
+import { TokenDto } from '../../models/token-dto';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidenav',
@@ -8,15 +11,35 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
   styleUrls: ['./sidenav.component.scss'],
 })
 export class SidenavComponent {
-  constructor(private oauthSvc: OauthService, public auth: AngularFireAuth) {}
+  socialUser: any;
+  constructor(
+    private oauthSvc: OauthService,
+    public auth: AngularFireAuth,
+    private tokenSvc: TokenService,
+    private router: Router
+  ) {}
 
   onGoogleLogin() {
     this.oauthSvc.signInWithGoogle().then(data => {
       console.log(data);
+      this.socialUser = data;
+      const tokenGoogle = new TokenDto(this.socialUser.idToken);
+      this.oauthSvc.google(tokenGoogle).subscribe({
+        next: (res: any) => {
+          console.log(res);
+          this.tokenSvc.setToken(res.value);
+          this.router.navigate(['/']);
+        },
+        error: (err: any) => {
+          console.log(err);
+          this.logout();
+        },
+      });
     });
   }
 
   logout() {
+    this.tokenSvc.logout();
     this.oauthSvc.logout();
   }
 }
--- turismo-app/src/app/services/oauth.service.ts
+++ turismo-app/src/app/services/oauth.service.ts
@@ -2,12 +2,33 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import firebase from 'firebase/compat/app';
 import { Router } from '@angular/router';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { TokenDto } from '../models/token-dto';
+import { Observable } from 'rxjs';
+
+const headers = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+};
 
 @Injectable({
   providedIn: 'root',
 })
 export class OauthService {
-  constructor(private afAuth: AngularFireAuth, private router: Router) {}
+  oauthURL = 'http://localhost:8080';
+
+  constructor(
+    private afAuth: AngularFireAuth,
+    private router: Router,
+    private http: HttpClient
+  ) {}
+
+  public google(tokenDto: TokenDto): Observable<TokenDto> {
+    return this.http.post<TokenDto>(
+      `${this.oauthURL}/oauth/google`,
+      tokenDto,
+      headers
+    );
+  }
 
   async signInWithGoogle() {
     try {
