50a58729a3ecb577b508b6dc5438f11b9b85537b
Nazareno Gabriel Bastanzo <b.nazarenogabriel@gmail.com>
2022-06-09 10:56:18 -0300

    Frontend: 1st approach hotels

--- turismo-app/src/app/pages/hotels/hotels.component.ts
+++ turismo-app/src/app/pages/hotels/hotels.component.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { BehaviorSubject, tap } from 'rxjs';
+import { ILocation } from 'src/app/interfaces/location.interface';
+import { FlightService } from '../../services/flight.service';
 
 @Component({
   selector: 'app-hotels',
   templateUrl: './hotels.component.html',
-  styleUrls: ['./hotels.component.scss']
+  styleUrls: ['./hotels.component.scss'],
 })
 export class HotelsComponent implements OnInit {
-
-  constructor() { }
+  myForm!: FormGroup;
+  locationsDestinySource = new BehaviorSubject<ILocation[]>([]);
+  locationsDestiny$ = this.locationsDestinySource.asObservable();
+  constructor(
+    private formBuilder: FormBuilder,
+    private flightSvc: FlightService
+  ) {}
 
   ngOnInit(): void {
+    this.myForm = this.initForm();
   }
 
+  initForm(): FormGroup {
+    return this.formBuilder.group({
+      destiny: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(50),
+        ],
+      ],
+    });
+  }
+
+  onTypingDestiny() {
+    if (this.myForm.get('destiny')?.value.length >= 3) {
+      this.flightSvc
+        .searchCityAndAirport(this.myForm.get('destiny')?.value, true)
+        .pipe(
+          tap((res: any) => {
+            // console.log(res.data);
+            this.locationsDestinySource.next(res.data);
+          })
+        )
+        .subscribe();
+    }
+  }
+
+  onSubmit() {
+    // ...TODO...
+  }
 }
